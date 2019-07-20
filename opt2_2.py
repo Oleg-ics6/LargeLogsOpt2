8d33ce0ff929aeec351f1d649197c4055b9d1d80
Oleg <apramov@mail.ru>
2019-07-20 22:47:21 +0300

    Add compress for the logs

---
+++ opt2_2.py
@@ -0,0 +1,28 @@
+import gzip
+import shutil
+import os
+import re
+from multiprocessing import Process
+
+
+def gzipper(*fnames):
+    for fname in fnames:
+        with open(fname, 'rb') as f_in:
+                with gzip.open(fname + '.gz', 'wb', 5) as f_out:
+                    shutil.copyfileobj(f_in, f_out, 20_000_000)
+
+
+fnames = [i for i in os.listdir() if re.match(r'log\d+', i)]
+count = 0
+procs = []
+
+
+for i in range(4):
+    proc = Process(target=gzipper, args=(fnames[count:count + 4]))
+    procs.append(proc)
+    proc.start()
+    count += 4
+
+
+for proc in procs:
+    proc.join()
